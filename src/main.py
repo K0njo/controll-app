4649656c374f5154dfcb32a7d70914b3a0a9958a
Egor Kononeko / K0njo <kononenko.egor2012@gmail.com>
2023-02-19 17:47:36 +0300

    feat:mediafile endpoints were added

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-
+from src.models.media_file.media_api import router_media
 from src.auth.authentication_api import router_auth
 from src.models.user import user_model
 from src.database_connection import engine
 
 app = FastAPI()
 app.include_router(router_auth)
+app.include_router(router_media)
 user_model.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
