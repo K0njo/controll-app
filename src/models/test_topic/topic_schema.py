2b0bb1ff0f200b6bf04d3a00bd9dbef1c9e2bd6a
Egor Kononeko / K0njo <kononenko.egor2012@gmail.com>
2023-02-22 09:45:46 +0300

    feat: topic validator was added

--- src/models/test_topic/topic_schema.py
+++ src/models/test_topic/topic_schema.py
@@ -1,6 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import Dict
 
 class TopicSchema(BaseModel):
     question: str
     answer: Dict[str, str]
+
+    @validator('answer')
+    def validator_answer(cls, value):
+        if len(value) != 4:
+            raise ValueError('Answer field must contain 4 answers ')
+        return value
