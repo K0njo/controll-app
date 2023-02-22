1f26be682809951188da63d4e3ac78733a9f6a5e
Egor Kononeko / K0njo <kononenko.egor2012@gmail.com>
2023-02-22 10:04:51 +0300

    feat: foreign keys for lesson model were added

--- src/models/lessons/lesson_model.py
+++ src/models/lessons/lesson_model.py
@@ -1,6 +1,7 @@
 from src.database_connection import Base
-from sqlalchemy import Integer, Column, String, Boolean
-
+from sqlalchemy import Integer, Column, String, Boolean, ForeignKey
+from src.models.media_file.medi_model import Media
+from src.models.test_topic.topic_model import  Topic
 
 class Lesson(Base):
     __tablename__ = 'lesson'
@@ -9,5 +10,5 @@ class Lesson(Base):
     level = Column(String(30))
     topic_name = Column(String(100))
     topic_description = Column(String(3000))
-    media_file_id = Column(Integer)
-    test_topic_id = Column(Integer)
+    media_file_id = Column(Integer, ForeignKey("media.id"))
+    test_topic_id = Column(Integer, ForeignKey("test_topic.id"))
