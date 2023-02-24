a7311b5b6e04722d1c30b1dcc944522e42507876
Anastasia <86727119+AnasnasiaKartashova@users.noreply.github.com>
2023-02-24 19:18:00 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from src.auth.authentication_api import router_auth
 from src.database_connection import engine, Base
 from src.models.books.book_api import router_book
+from src.models.test_topic.topic_api import router_topic
 
 app = FastAPI()
 
