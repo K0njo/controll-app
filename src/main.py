6232004ae01197de1d8edeccb16b7eb1aa384214
Anastasia <86727119+AnasnasiaKartashova@users.noreply.github.com>
2023-02-24 19:28:26 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 
 from src.auth.authentication_api import router_auth
+from src.database_connection import engine, Base
+from src.models.books.book_api import router_book
+from src.models.test_topic.topic_api import router_topic
 from src.models.lessons.lesson_api import router_lessons
-from src.models.user import user_model
-from src.database_connection import engine
 
 app = FastAPI()
+
 app.include_router(router_auth)
+app.include_router(router_book)
+app.include_router(router_topic)
 app.include_router(router_lessons)
-user_model.Base.metadata.create_all(bind=engine)
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
