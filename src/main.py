45c13e3fc94eab1141b2e53a4333f8000ec20b37
Anastasia <86727119+AnasnasiaKartashova@users.noreply.github.com>
2023-02-24 20:29:59 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,20 +2,12 @@ from fastapi import FastAPI
 
 from src.auth.authentication_api import router_auth
 from src.database_connection import engine, Base
-from src.models.books.book_api import router_book
-from src.models.test_topic.topic_api import router_topic
-from src.models.lessons.lesson_api import router_lessons
-from src.models.test_level.level_api import router_level_test
 from src.models.media_file.media_api import router_media
 
 
 app = FastAPI()
 
 app.include_router(router_auth)
-app.include_router(router_book)
-app.include_router(router_topic)
-app.include_router(router_lessons)
-app.include_router(router_level_test)
 app.include_router(router_media)
 
 
